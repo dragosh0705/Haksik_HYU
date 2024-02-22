aa9c8d01636569f40ce89997dc076472d3f8ea23
dragosh0705 <pingu0705@hanyang.ac.kr>
2024-02-22 18:46:49 +0900

    feat:cocoapods download and layout done

---
+++ Haksik_HYU/Model/TodayMenuStruct.swift
@@ -0,0 +1,8 @@
+//
+//  TodayMenuStruct.swift
+//  Haksik_HYU
+//
+//  Created by 최영우 on 2024. 2. 22..
+//
+
+import Foundation
---
+++ Haksik_HYU/View/todayTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  todayTableViewCell.swift
+//  Haksik_HYU
+//
+//  Created by 최영우 on 2024. 2. 22..
+//
+
+import UIKit
+
+class todayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
